9a942af047115ea95327e5c01e5a42c1b42190b5
VienNguyen <luongvien13t2@gmail.com>
2018-03-27 14:27:46 +0700

    test prettier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import express from "express";
+import express   from "express";
 
-const app = express() ;
+const app =  express() ;
 
 app.listen(5555, () => {
   console.log("Express server started at port 5555");
@@ -8,7 +8,7 @@ app.listen(5555, () => {
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", (req, res) => {
-  console.log("GET /");
-  console.log("GET ////");
+  console.log("GET /")
+  console.log("GET ////")
   res.send("hello world");
 });
